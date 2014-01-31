caad84bc96f7d93f60e9e82dfacd5785c42fdf55
K-RKidd <kr248196@muhlenberg.edu>
2014-01-31 09:59:28 -0500

    Initial Commit

---
+++ Quiz3/WhereamiAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WhereamiAppDelegate.h
+//  Quiz3
+//
+//  Created by Krystle on 1/31/14.
+//  Copyright (c) 2014 Krystle Kidd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WhereamiAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
